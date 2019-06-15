8da3c432b9b039cf5e32fb68a803c8b585fcb34f
Hiroto Yoshidome <kichiru.3161026@gmail.com>
2019-06-15 21:15:33 +0900

    スクレイピングのサンプルを追加

--- src/main/java/ScrapeMain.java
+++ src/main/java/ScrapeMain.java
@@ -1,5 +1,19 @@
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+/**
+ * 
+ * @author hiroto
+ *
+ */
 public class ScrapeMain {
-	public static void main(String[] args) {
-		System.out.println("hello");
+	public static void main(String[] args) throws IOException {
+		String url = "https://github.com/";
+		Document document = Jsoup.connect(url).get();
+		Elements elements = document.select("h3");
+		System.out.println(elements);
 	}
 }

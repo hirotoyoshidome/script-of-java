2005eb018f1cb98cf97efd78cc2d61451df092d6
Hiroto Yoshidome <kichiru.3161026@gmail.com>
2019-06-16 00:40:29 +0900

    csvファイルの読み込み処理を追加

--- src/main/java/jp/yoru722/java/script/CSVForm.java
+++ src/main/java/jp/yoru722/java/script/CSVForm.java
@@ -1,5 +1,7 @@
 package jp.yoru722.java.script;
 
+import com.opencsv.bean.CsvBindByName;
+
 import lombok.Data;
 
 /**
@@ -8,9 +10,47 @@ import lombok.Data;
  * @author hiroto
  *
  */
+// eclipseに読み込ませないと使用できないためgetter, setter書く
 @Data
 public class CSVForm {
+	@CsvBindByName(column = "id", required = true)
+	private String id;
+	@CsvBindByName(column = "name", required = true)
 	private String name;
-	private String age;
-	private String gender;
+	@CsvBindByName(column = "price", required = true)
+	private String price;
+	@CsvBindByName(column = "num", required = true)
+	private String num;
+
+	public String getId() {
+		return this.id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPrice() {
+		return this.price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+	public String getNum() {
+		return this.num;
+	}
+
+	public void setNum(String num) {
+		this.num = num;
+	}
 }
--- src/main/java/jp/yoru722/java/script/CSVReader.java
+++ src/main/java/jp/yoru722/java/script/CSVReader.java
@@ -1,8 +1,24 @@
 package jp.yoru722.java.script;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
 public class CSVReader {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// CSV読み込み処理
-		CSVForm form = new CSVForm();
+		Reader reader = Files.newBufferedReader(Paths.get("files/test.csv"));
+		CsvToBean<CSVForm> csvToBean = new CsvToBeanBuilder<CSVForm>(reader).withType(CSVForm.class).build();
+		List<CSVForm> items = csvToBean.parse();
+
+		// java8移行のλ式
+		items.forEach(i -> {
+			System.out.println(i.getName());
+		});
 	}
 }
